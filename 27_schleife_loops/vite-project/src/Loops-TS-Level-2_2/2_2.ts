f971ff02c0b00103db35d135d0640288e2ad8b19
pablobeckg <beckgarreaud@gmail.com>
2024-05-13 10:12:53 +0200

    push

--- 27_schleife_loops/vite-project/src/Loops-TS-Level-2_2/2_2.ts
+++ 27_schleife_loops/vite-project/src/Loops-TS-Level-2_2/2_2.ts
@@ -3,9 +3,10 @@ const bttn = document.querySelector('#loop_bttn');
 let text = document.querySelector('p')!;
 
 if (numInput && bttn && text) {
-    bttn.addEventListener('click', () => {
-        let numInputVal = parseInt(numInput.value);
+    bttn.addEventListener('click', (event: Event) => {
         
+        let numInputVal = parseInt(numInput.value);
+        event.preventDefault();
         for (let index = 1; index <= numInputVal; index++) {
            text.textContent = 'L' + 'o'.repeat(index) + 'p';
         }
